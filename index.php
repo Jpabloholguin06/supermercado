59ccc4a46ed5f98747895d695940d40fa82f146d
pabloholguin2844 <61478936+pabloholguin2844@users.noreply.github.com>
2020-09-09 16:17:13 -0500

    Supermercado

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supermercados</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body class="bg-dark text-white">
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">
+            <img src="img/vaca.png" width="30" height="30" class="d-inline-block align-top" alt="Logo" loading="lazy">
+        SUPERMERCADO
+        </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </ul>
+  </div>
+</nav>
+    </header>
+    <main>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-4">
+                <form>
+                <h2 class="text-center mb-4">Factura de Compra</h2>
+
+                <div class="row mb-2">
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Producto 1">
+                    </div>
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Precio($)">
+                    </div>
+                </div>
+                
+                <div class="row mb-2">
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Producto 2">
+                    </div>
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Precio($)">
+                    </div>
+                </div>
+                
+                <div class="row mb-2">
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Producto 3">
+                    </div>
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Precio($)">
+                    </div>
+                </div>
+                
+                <div class="row mb-2">
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Producto 4">
+                    </div>
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Precio($)">
+                    </div>
+                </div>
+
+                <div class="row mb-2">
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Producto 5">
+                    </div>
+                    <div class="col">
+                    <input type="text" class="form-control" placeholder="Precio($)">
+                    </div>
+                </div>
+
+                <h1 class="mb-3"></h1>
+                <button class="btn btn-primary btn-block" type="submit">Calcular</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    </main>
+    <footer>
+    
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
+
